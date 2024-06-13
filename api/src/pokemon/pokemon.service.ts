835ad35963ad9bd2cd8e135614cc1955d66fb19c
Nelson Peixoto Lopes <nelson.npl@gmail.com>
2024-06-13 22:40:42 +0000

    feat: Add PokemonService methods for listing and getting Pokemon

--- api/src/pokemon/pokemon.service.ts
+++ api/src/pokemon/pokemon.service.ts
@@ -1,4 +1,27 @@
 import { Injectable } from "@nestjs/common";
+import { HttpService } from "@nestjs/axios";
+import { firstValueFrom } from "rxjs";
+import { List } from "../interfaces/list.interface";
 
 @Injectable()
-export class PokemonService {}
+export class PokemonService {
+  private readonly url = "https://pokeapi.co/api/v2/pokemon";
+
+  constructor(private httpService: HttpService) {}
+
+  async list(offset: number = 0, limit: number = 20): Promise<List> {
+    const url = `${this.url}?offset=${offset}&limit=${limit}`;
+    const { data } = await firstValueFrom(this.httpService.get(url));
+    const list: List = {
+      count: data.count,
+      results: data.results,
+    };
+    return list;
+  }
+
+  async get(filter: string): Promise<any> {
+    const url = `${this.url}/${filter}`;
+    const response = await firstValueFrom(this.httpService.get(url));
+    return response.data;
+  }
+}
