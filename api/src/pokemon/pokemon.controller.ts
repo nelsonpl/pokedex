ab849d95f6964783bac03e9f3a5b4029f15052ab
Nelson Peixoto Lopes <nelson.npl@gmail.com>
2024-06-13 22:40:56 +0000

    Add API endpoints for listing and searching Pokemon

--- api/src/pokemon/pokemon.controller.ts
+++ api/src/pokemon/pokemon.controller.ts
@@ -1,5 +1,22 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Get, Param, Query } from "@nestjs/common";
+import { PokemonService } from "./pokemon.service";
+import { List } from "../interfaces/list.interface";
 
-@Controller('pokemon')
+@Controller("pokemon")
 export class PokemonController {
+  constructor(private readonly pokemonService: PokemonService) {}
+
+  @Get()
+  async getPokemons(
+    @Query("offset") offset: number = 0,
+    @Query("limit") limit: number = 20
+  ): Promise<List> {
+    return this.pokemonService.list(offset, limit);
+  }
+
+
+  @Get(":filter")
+  async searchPokemon(@Param("filter") filter: string) {
+    return this.pokemonService.get(filter);
+  }
 }
