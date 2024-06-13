dd953e37ca0cdd654c3ef0bb7b9a8700356d4105
Nelson Peixoto Lopes <nelson.npl@gmail.com>
2024-06-13 22:43:21 +0000

    refactor: Update PokemonService to use HttpClient for making HTTP requests

--- app/src/app/pokemon/pokemon.service.ts
+++ app/src/app/pokemon/pokemon.service.ts
@@ -1,4 +1,20 @@
-import { Injectable } from "@angular/core";
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Pokemon } from '../interfaces/pokemon.interface';
+import { List } from '../interfaces/list.interface';
 
 @Injectable()
-export class PokemonService {}
+export class PokemonService {
+  private apiUrl = 'http://localhost:3000/pokemon';
+
+  constructor(private http: HttpClient) {}
+
+  list(offset: number = 0, limit: number = 20): Observable<List> {
+    return this.http.get<any>(`${this.apiUrl}?offset=${offset}&limit=${limit}`);
+  }
+
+  get(id: number): Observable<Pokemon> {
+    return this.http.get<Pokemon>(`${this.apiUrl}/${id}`);
+  }
+}
