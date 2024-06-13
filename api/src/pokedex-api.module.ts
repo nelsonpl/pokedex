70c78b82205343964b5cec133bb5ba4e61b574fc
Nelson Peixoto Lopes <nelson.npl@gmail.com>
2024-06-13 22:41:53 +0000

    feat: Add HttpModule to Pokedex API module for making HTTP requests

--- api/src/pokedex-api.module.ts
+++ api/src/pokedex-api.module.ts
@@ -1,9 +1,10 @@
-import { Module } from '@nestjs/common';
-import { PokemonController } from './pokemon/pokemon.controller';
-import { PokemonService } from './pokemon/pokemon.service';
+import { Module } from "@nestjs/common";
+import { PokemonController } from "./pokemon/pokemon.controller";
+import { PokemonService } from "./pokemon/pokemon.service";
+import { HttpModule } from "@nestjs/axios";
 
 @Module({
-  imports: [],
+  imports: [HttpModule],
   controllers: [PokemonController],
   providers: [PokemonService],
 })
