4ba1f7ba048ce091f8366933c3d1fee8a9273d3a
Nelson Peixoto <nelson.npl@gmail.com>
2024-06-13 19:48:23 -0400

    refactor: Update PokedexAppModule imports and providers

--- app/src/app/pokedex-app.module.ts
+++ app/src/app/pokedex-app.module.ts
@@ -2,19 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 
-import { routes } from './pokedex-app.routes';
-import { PokedexAppComponent } from './pokedex-app.component';
-import { PokemonService } from './pokemon/pokemon.service';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { provideHttpClient } from '@angular/common/http';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { PokemonService } from './pokemon/pokemon.service';
+
+import { routes } from './pokedex-app.routes';
+import { PokedexAppComponent } from './pokedex-app.component';
 
 @NgModule({
   imports: [
     BrowserModule,
     RouterModule.forRoot(routes),
     BrowserAnimationsModule,
+    MatToolbarModule,
+
+
   ],
-  providers: [PokemonService, provideHttpClient()],
+  providers: [PokemonService, provideHttpClient(), provideAnimationsAsync()],
   declarations: [PokedexAppComponent],
   bootstrap: [PokedexAppComponent],
 })
