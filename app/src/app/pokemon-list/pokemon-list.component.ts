bec9935bab953f9f06bd5e6242d9c5a8bed48436
Nelson Peixoto <nelson.npl@gmail.com>
2024-06-13 19:47:55 -0400

    refactor: Update PokemonListComponent to use Material Design components for displaying the list of Pokemon

--- app/src/app/pokemon-list/pokemon-list.component.ts
+++ app/src/app/pokemon-list/pokemon-list.component.ts
@@ -2,11 +2,15 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { PokemonService } from '../pokemon/pokemon.service';
 import { CommonModule } from '@angular/common';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatList, MatListModule } from '@angular/material/list';
+import { MatDivider, MatDividerModule } from '@angular/material/divider';
 
 @Component({
   selector: 'app-pokemon-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatGridListModule, MatCardModule, MatListModule, MatDividerModule],
   providers: [],
   templateUrl: './pokemon-list.component.html',
   styleUrl: './pokemon-list.component.scss',
@@ -14,7 +18,7 @@ import { CommonModule } from '@angular/common';
 export class PokemonListComponent implements OnInit {
   list: any[] = [];
   offset: number = 0;
-  limit: number = 1000;
+  limit: number = 20;
   showLoadMore: boolean = true;
 
   constructor(private service: PokemonService, private router: Router) {}
