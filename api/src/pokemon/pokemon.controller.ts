a517d41951a7d6c78d0630bcfb7a51352d88bd3c
Nelson <nelson.npl@gmail.com>
2024-06-14 12:54:58 -0400

    refactor: Add endpoint to get Pokemon by name

--- api/src/pokemon/pokemon.controller.ts
+++ api/src/pokemon/pokemon.controller.ts
@@ -7,16 +7,20 @@ export class PokemonController {
   constructor(private readonly pokemonService: PokemonService) {}
 
   @Get()
-  async getPokemons(
+  async list(
     @Query("offset") offset: number = 0,
     @Query("limit") limit: number = 20
   ): Promise<List> {
     return this.pokemonService.list(offset, limit);
   }
 
+  @Get(":id")
+  async getById(@Param("id") id: number) {
+    return this.pokemonService.getById(id);
+  }
 
-  @Get(":filter")
-  async searchPokemon(@Param("filter") filter: string) {
-    return this.pokemonService.get(filter);
+  @Get("/get-by-name/:name")
+  async getByName(@Param("name") name: string) {
+    return this.pokemonService.getByName(name);
   }
 }
