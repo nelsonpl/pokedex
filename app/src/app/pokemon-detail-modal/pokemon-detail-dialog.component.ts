ca893101ceb3851be447f20df7198f92ab7e1986
Nelson <nelson.npl@gmail.com>
2024-06-14 12:51:50 -0400

    refactor: Update Pokemon Detail Dialog component and template

--- app/src/app/pokemon-detail-modal/pokemon-detail-dialog.component.ts
+++ app/src/app/pokemon-detail-modal/pokemon-detail-dialog.component.ts
@@ -1,6 +1,13 @@
-import {ChangeDetectionStrategy, Component, OnInit, inject, model, signal} from '@angular/core';
-import {FormsModule} from '@angular/forms';
-import {MatButtonModule} from '@angular/material/button';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  OnInit,
+  inject,
+  model,
+  signal,
+} from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
 import {
   MAT_DIALOG_DATA,
   MatDialog,
@@ -10,8 +17,8 @@ import {
   MatDialogRef,
   MatDialogTitle,
 } from '@angular/material/dialog';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { ActivatedRoute } from '@angular/router';
 import { Pokemon } from '../interfaces/pokemon.interface';
 import { PokemonService } from '../pokemon/pokemon.service';
@@ -36,7 +43,6 @@ export interface DialogData {
     MatDialogContent,
     MatDialogActions,
     MatDialogClose,
-    MatCardModule
   ],
 })
 export class DialogOverviewExampleDialog implements OnInit {
@@ -44,14 +50,16 @@ export class DialogOverviewExampleDialog implements OnInit {
   readonly dialogRef = inject(MatDialogRef<DialogOverviewExampleDialog>);
   readonly data = inject<DialogData>(MAT_DIALOG_DATA);
 
-  constructor(private route: ActivatedRoute, private service: PokemonService) {}
+  constructor(private service: PokemonService) {}
 
   ngOnInit(): void {
     this.getPokemon();
   }
 
   getPokemon(): void {
-    this.service.get(this.data.id).subscribe((pokemon) => (this.pokemon = pokemon));
+    this.service
+      .getById(this.data.id)
+      .subscribe((pokemon) => (this.pokemon = pokemon));
   }
 
   onNoClick(): void {
