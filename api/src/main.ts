9805680cc6206bafe44a67b6bb34329cb0a21101
Nelson <nelson.npl@gmail.com>
2024-06-14 16:51:42 -0400

    refactor: Add NestJS configuration for environment variables and validation

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,11 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { PokedexApiModule } from './pokedex-api.module';
+import { ValidationPipe } from '@nestjs/common';
+import config from './config';
 
 async function bootstrap() {
   const app = await NestFactory.create(PokedexApiModule);
+
+  app.useGlobalPipes(new ValidationPipe());
   app.setGlobalPrefix('api');
   app.enableCors();
-  await app.listen(3000);
+  
+  await app.listen(config().port);
 }
 
 bootstrap();
--- api/src/pokedex-api.module.ts
+++ api/src/pokedex-api.module.ts
@@ -2,9 +2,15 @@ import { Module } from "@nestjs/common";
 import { PokemonController } from "./pokemon/pokemon.controller";
 import { PokemonService } from "./pokemon/pokemon.service";
 import { HttpModule } from "@nestjs/axios";
+import { ConfigModule } from "@nestjs/config";
 
 @Module({
-  imports: [HttpModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    HttpModule,
+  ],
   controllers: [PokemonController],
   providers: [PokemonService],
 })
