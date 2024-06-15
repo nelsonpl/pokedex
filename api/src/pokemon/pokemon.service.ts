ceed499b73982442628532985d269231dd729215
Nelson Peixoto <nelson.npl@gmail.com>
2024-06-15 08:33:10 -0400

    refactor: Update PokemonService to handle Pokemon not found errors

--- api/src/pokemon/pokemon.service.ts
+++ api/src/pokemon/pokemon.service.ts
@@ -2,6 +2,8 @@ import { Injectable, NotFoundException } from "@nestjs/common";
 import { HttpService } from "@nestjs/axios";
 import { firstValueFrom } from "rxjs";
 import { List } from "../interfaces/list.interface";
+import { AxiosResponse } from "axios";
+import { Pokemon } from "src/interfaces/pokemon.interface";
 
 @Injectable()
 export class PokemonService {
@@ -11,33 +13,70 @@ export class PokemonService {
 
   async list(offset: number = 0, limit: number = 20): Promise<List> {
     const url = `${this.url}?offset=${offset}&limit=${limit}`;
-    const { data } = await firstValueFrom(this.httpService.get(url));
+    let response: AxiosResponse<any, any>;
+
+    try {
+      response = await firstValueFrom(this.httpService.get(url));
+    } catch {
+      throw new NotFoundException("Sorry, try again later.");
+    }
+
+    const data = response.data;
     const list: List = {
       count: data.count,
-      results: data.results,
+      results: data.results.map((item: any) => ({
+        name: item.name,
+        url: item.url,
+      })),
     };
     return list;
   }
 
-  async getById(id: number): Promise<any> {
+  async getById(id: number): Promise<Pokemon> {
     const url = `${this.url}/${id}`;
-    let response;
+    let response: AxiosResponse<any, any>;
+
     try {
       response = await firstValueFrom(this.httpService.get(url));
     } catch {
       throw new NotFoundException("Pokemon not found");
     }
-    return response.data;
+
+    const data = response.data;
+    const pokemon: Pokemon = {
+      id: data.id,
+      name: data.name,
+      base_experience: data.base_experience,
+      height: data.height,
+      weight: data.weight,
+      sprites: {
+        front_default: data.sprites.front_default,
+      },
+    };
+    return  pokemon;
   }
 
-  async getByName(name: string): Promise<any> {
+  async getByName(name: string): Promise<Pokemon> {
     const url = `${this.url}/${name}`;
-    let response;
+    let response: AxiosResponse<any, any>;
+
     try {
       response = await firstValueFrom(this.httpService.get(url));
     } catch {
       throw new NotFoundException("Pokemon not found");
     }
-    return response.data;
+    
+    const data = response.data;
+    const pokemon: Pokemon = {
+      id: data.id,
+      name: data.name,
+      base_experience: data.base_experience,
+      height: data.height,
+      weight: data.weight,
+      sprites: {
+        front_default: data.sprites.front_default,
+      },
+    };
+    return  pokemon;
   }
 }
