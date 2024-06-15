8d2b230783e7fb6855ea5d8b03cf396c0b4d4670
Nelson Peixoto <nelson.npl@gmail.com>
2024-06-14 21:13:05 -0400

    refactor: Remove unused Docker-related files and scripts

--- app/deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-echo "Building the Docker image..."
-docker build -t pokedex-app .
-
-echo "Running the Docker container with npm start..."
-docker run --rm -p 4200:4200 pokedex-app npm start
