fdd772890fb9aa4a5f4bef32278a0a1ef7b28dd9
Nelson Peixoto <nelson.npl@gmail.com>
2024-06-14 21:13:14 -0400

    refactor: Update Docker build command to include --no-cache and --compress options

--- api/deploy.sh
+++ api/deploy.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+
+# Exit immediately if a command exits with a non-zero status
 set -e
 
 echo "Building the Docker image..."
-docker build -t pokedex-api .
+docker build --no-cache --compress -t pokedex-api .
 
 echo "Running the Docker container with npm start..."
 docker run --rm -p 3000:3000 pokedex-api npm start
